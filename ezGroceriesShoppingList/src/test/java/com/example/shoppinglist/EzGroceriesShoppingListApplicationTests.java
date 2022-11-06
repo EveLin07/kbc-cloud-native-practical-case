0b2e1217b89e63b914029afaff5180667b007a22
Evelin Dicheva <evelin.dicheva@kbc.com>
2022-11-06 19:14:13 +0200

    create ezGroceriesShoppingList project

---
+++ ezGroceriesShoppingList/src/test/java/com/example/shoppinglist/EzGroceriesShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzGroceriesShoppingListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
