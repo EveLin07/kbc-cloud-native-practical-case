6307ea5575b0d03d7e68a45cb285c63fa8bb0e10
Evelin Dicheva <evelin.dicheva@kbc.com>
2022-11-28 09:53:32 +0200

    adding logging

--- ezGroceriesShoppingList/src/main/java/com/example/shoppinglist/web/CocktailController.java
+++ ezGroceriesShoppingList/src/main/java/com/example/shoppinglist/web/CocktailController.java
@@ -2,6 +2,7 @@ package com.example.shoppinglist.web;
 
 import com.example.shoppinglist.data.CocktailDataProvider;
 import com.example.shoppinglist.model.Cocktail;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
+@Slf4j
 public class CocktailController {
 
     @GetMapping("/cocktails")
@@ -20,6 +22,7 @@ public class CocktailController {
     public List<Cocktail> getCocktails(@RequestParam(value = "search", required = false) String search) {
 
         List<Cocktail> cocktails = CocktailDataProvider.getCocktails();
+        log.info("Getting cocktails:...");
         return Objects.isNull(search)
                 ? cocktails
                 : cocktails.stream().filter(cocktail -> cocktail.getName().contains(search))
