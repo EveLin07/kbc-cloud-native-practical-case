a018bc1804d0124d02ed22b82b1f56048a40f85c
Evelin Dicheva <evelin.dicheva@kbc.com>
2022-11-28 12:11:48 +0200

    lab-03-external-api-external-api-cleanup

--- ezGroceriesShoppingList/src/main/java/com/example/shoppinglist/web/CocktailController.java
+++ ezGroceriesShoppingList/src/main/java/com/example/shoppinglist/web/CocktailController.java
@@ -1,7 +1,5 @@
 package com.example.shoppinglist.web;
 
-import com.example.shoppinglist.data.CocktailDataProvider;
-import com.example.shoppinglist.model.Cocktail;
 import com.example.shoppinglist.model.CocktailDBResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,9 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -27,8 +22,7 @@ public class CocktailController {
     @ResponseStatus(HttpStatus.OK)
     public CocktailDBResponse getCocktails(@RequestParam(value = "search", required = false) String search) {
 
-        var result = cocktailDBClient.searchCocktails(search);
-        return result;
+       return cocktailDBClient.searchCocktails(search);
     }
 
 }
